2887ad433292918124d14cb7a4626fa8971578d6
KhanaghaNoori <kn.khannoori@gmail.com>
2024-12-02 12:41:17 +0100

    firs Twitt

---
+++ my-tweet-react-app/src/main.jsx
@@ -0,0 +1,14 @@
+// import { render } from 'preact'
+// import './index.css'
+// import { App } from './app.jsx'
+
+// render(<App />, document.getElementById('app'))
+
+
+// Template
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.jsx';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
